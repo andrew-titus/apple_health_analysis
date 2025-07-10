093e98301d6ab9a4b0e34595a6282c6cb37e4fbb
Andrew Titus <drewtitus59@gmail.com>
2025-07-10 17:16:39 -0400

    Add src/ directory; start Glucose notebook

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,3 +1,10 @@
 #!/bin/bash -euxo pipefail
+echo "Checking src..."
+uvx ruff format src/
+uvx ruff check --fix src/
+
+echo "Checking notebooks..."
 uvx ruff format notebooks/
 uvx ruff check --fix notebooks/
+
+echo "DONE!"
---
+++ src/apple_health_analysis/export.py
@@ -0,0 +1,19 @@
+import os
+import xml.etree.ElementTree as ET
+
+# ../../../apple_health
+APPLE_HEALTH_DIR = os.path.join(
+    os.path.dirname(os.path.dirname(os.path.dirname(__file__))), "apple_health"
+)
+EXPORT = os.path.join(APPLE_HEALTH_DIR, "export.xml")
+if not os.path.isfile(EXPORT):
+    raise FileNotFoundError(f"Did not find export.xml in {APPLE_HEALTH_DIR}")
+
+
+def parse_export() -> ET.Element:
+    """Parse the export.xml file into a xml.etree.ElementTree.Element file."""
+    export_tree = ET.parse(EXPORT)
+    export_root = export_tree.getroot()
+    if export_root.tag != "HealthData":
+        raise ValueError(f"Unable to find HealthData tag in {EXPORT}")
+    return export_root
