f5496d1ad4fa03977e5f19ce917e4696648679e9
Andrew Titus <drewtitus59@gmail.com>
2025-07-11 16:44:40 -0400

    Add output clearing to checks

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,6 +1,10 @@
 #!/bin/bash -euxo pipefail
+echo "Clearing notebook outputs..."
+uv run --with jupyter jupyter nbconvert --clear-output --inplace notebooks/*
+
+echo "Running ruff..."
 for subdir in src tests notebooks; do
-    echo "Checking ${subdir}..."
+    echo "-> Checking ${subdir}..."
     uvx ruff format "${subdir}/"
     uvx ruff check --fix "${subdir}/"
 done
