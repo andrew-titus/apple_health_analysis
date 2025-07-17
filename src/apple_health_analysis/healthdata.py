59bbe83d487d85a8f74c197d56abdb7aac2c0d7d
Andrew Titus <drewtitus59@gmail.com>
2025-07-17 14:11:49 -0400

    Add caching logic for HealthData; not tested yet

--- src/apple_health_analysis/healthdata.py
+++ src/apple_health_analysis/healthdata.py
@@ -1,3 +1,4 @@
+import logging
 import os
 import xml.etree.ElementTree as ET
 
@@ -6,6 +7,10 @@ from pydantic import BaseModel, Field
 from tqdm import tqdm
 
 from apple_health_analysis.record import RECORD_TAG, Record
+
+
+logging.basicConfig(format="%(asctime)s - %(levelname)s - %(message)s")
+logger = logging.getLogger()
 
 
 # ../../../apple_health
@@ -31,14 +36,12 @@ class HealthData(BaseModel):
     # TODO: other things like ClinicalRecord?
 
     @classmethod
-    def from_xml_export(
-        cls, export: os.PathLike, show_tqdm: bool = True
-    ) -> "HealthData":
-        """Parse an XML export file into a HealthData object.
+    def from_xml_export(cls, export: os.PathLike, verbose: bool = True) -> "HealthData":
+        """Parse an XML export file into a HealthData object."""
+        logger.setLevel(logging.DEBUG if verbose else logging.INFO)
+        logger.debug("Creating HealthData from export XML file.")
 
-        Can optionally show progress with a [tqdm](https://github.com/tqdm/tqdm) progress bar.
-        Defaults to True.
-        """
+        logger.debug("Reading export XML file...")
         export_tree = ET.parse(export)
         export_root = export_tree.getroot()
         if export_root.tag != "HealthData":
@@ -46,20 +49,83 @@ class HealthData(BaseModel):
 
         records_list: list[Record] = list()
 
-        elem_iter = tqdm(export_root) if show_tqdm else export_root
+        logger.debug("Parsing elements from export XML file...")
+        elem_iter = tqdm(export_root) if verbose else export_root
         for elem in elem_iter:
             # TODO: handle other tags as well
             if elem.tag == RECORD_TAG:
                 records_list.append(Record.from_xml_element(elem))
 
-        return cls(
-            records=pd.DataFrame([dict(record) for record in records_list]),
-        )
+        logger.debug("Converting records to dictionaries...")
+        records_dicts: list[dict] = list()
+        records_iter = tqdm(records_list) if verbose else records_list
+        for record in records_iter:
+            records_dicts.append(dict(record))
+
+        logger.debug("Converting record dictionaries to DataFrame...")
+        records_df = pd.DataFrame(records_dicts)
+
+        logger.debug("Creating HealthData...")
+        return cls(records=records_df)
+
+    def save_cache(self, cache: os.PathLike, verbose: bool = True) -> None:
+        """Save this object to a cache for future reuse."""
+        logger.setLevel(logging.DEBUG if verbose else logging.INFO)
+        logger.debug("Saving HealthData to cache at %s", cache)
+
+        if os.path.exists(cache):
+            raise FileExistsError(
+                f"Cache already exists at {cache}; delete before saving"
+            )
+        logger.debug("Creating cache directory at %s...", cache)
+        os.makedirs(cache)
+
+        records_jsonl = os.path.join(cache, "records.jsonl")
+        logger.debug("Saving records to cache at %s...", records_jsonl)
+        self.records.to_json(records_jsonl, orient="records", lines=True)
+
+        logger.debug("DONE saving HealthData to cache at %s", cache)
+
+    @classmethod
+    def from_cache(cls, cache: os.PathLike, verbose: bool = True) -> "HealthData":
+        """Load a cached HealthData object."""
+        logger.setLevel(logging.DEBUG if verbose else logging.INFO)
+        logger.debug("Loading HealthData from cache at %s", cache)
+
+        if not os.path.isdir(cache):
+            raise FileNotFoundError(f"Cache not found at {cache}")
+
+        records_jsonl = os.path.join(cache, "records.jsonl")
+        if not os.path.isfile(records_jsonl):
+            raise FileNotFoundError(f"Records JSONL not found at {records_jsonl}")
+
+        logger.debug("Loading records from cache at %s...", records_jsonl)
+        records_df = pd.read_json(records_jsonl, orient="records", lines=True)
+
+        logger.debug("Creating HealthData...")
+        return cls(records=records_df)
 
 
-def get_healthdata() -> HealthData:
+def get_healthdata(verbose: bool = True, use_cache: bool = True) -> HealthData:
     """Parse the export.xml file in the Apple Health directory into a HealthData object."""
-    EXPORT = os.path.join(APPLE_HEALTH_DIR, "export.xml")
-    if not os.path.isfile(EXPORT):
+    logger.setLevel(logging.DEBUG if verbose else logging.INFO)
+
+    cache = os.path.join(APPLE_HEALTH_DIR, "export_cache")
+    if os.path.isdir(cache):
+        if use_cache:
+            logger.info("Using cache found at %s", cache)
+            return HealthData.from_cache(cache)
+        else:
+            logger.info("Ignoring cache found at %s, since use_cache=False", cache)
+    else:
+        logger.info("No cache found at %s; creating from scratch")
+
+    export = os.path.join(APPLE_HEALTH_DIR, "export.xml")
+    if not os.path.isfile(export):
         raise FileNotFoundError(f"Did not find export.xml in {APPLE_HEALTH_DIR}")
-    return HealthData.from_xml_export(EXPORT)
+
+    healthdata = HealthData.from_xml_export(export, verbose=verbose)
+
+    healthdata.save_cache(cache, verbose=verbose)
+
+    return healthdata
